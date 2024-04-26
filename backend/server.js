261e4bf1ca728424664f6b217df4a61cb0f5919c
manuelMarkDenver <manuel.markdenver@gmail.com>
2024-04-27 01:10:59 +0800

    updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDb from "./config/db.js";
 
-import agentRoutes from "./routes/agent-routes.js";
+import agentsRoutes from "./routes/agent-routes.js";
+import callersRoutes from "./routes/caller-routes.js";
+import callsQueueRoutes from './routes/calls-queue-routes.js'
+
+import { notFound, errorHandler } from "./middleware/error-middleware.js";
 
 const port = process.env.PORT || 5000;
 
+connectDb();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("api running"));
 app.get("/api", (req, res) => res.send("api running"));
 
-app.use("/api/agents", agentRoutes);
+app.use("/api/v1/agents", agentsRoutes);
+app.use("/api/v1/callers", callersRoutes);
+app.use("/api/v1/queue", callsQueueRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));

843b46962d7d8d77dbbfff33ac1a20353e0dbf7d
manuelMarkDenver <manuel.markdenver@gmail.com>
2024-04-30 03:32:05 +0800

    added lines for serving static files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors'
+import cors from "cors";
 
 dotenv.config();
 import connectDb from "./config/db.js";
 
 import agentsRoutes from "./routes/agent-routes.js";
 import callersRoutes from "./routes/caller-routes.js";
-import callsQueueRoutes from './routes/calls-queue-routes.js'
+import callsQueueRoutes from "./routes/calls-queue-routes.js";
 
 import { notFound, errorHandler } from "./middleware/error-middleware.js";
 
@@ -17,18 +18,28 @@ connectDb();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.send("api running"));
-app.get("/api", (req, res) => res.send("api running"));
-333+6
 app.use("/api/v1/agents", agentsRoutes);
 app.use("/api/v1/callers", callersRoutes);
 app.use("/api/v1/queue", callsQueueRoutes);
 
-app.use(notFound);    
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
